a64c07c2760ba5311285bdb8c30f48b9c05b3e9b
João Pedro <joaopjt@icloud.com>
2023-09-13 00:10:38 -0300

    added w() method values

--- src/index.js
+++ src/index.js
@@ -972,7 +972,7 @@ class TaxiCab {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['twenty'],
       web: []
     };
     
@@ -988,19 +988,19 @@ class TaxiCab {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['bamboo'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['gold'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['people'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['heart'];
           break;
       }
     }
