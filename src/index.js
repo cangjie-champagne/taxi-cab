69622e37f20bae00326de26ec0ffe049a422be71
João Pedro <joaopjt@icloud.com>
2023-09-12 23:58:22 -0300

    added k() method values

--- src/index.js
+++ src/index.js
@@ -496,7 +496,7 @@ class TaxiCab {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['gold'],
       web: []
     };
     
@@ -512,19 +512,19 @@ class TaxiCab {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['corpse'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['bamboo'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['moon'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['mountain'];
           break;
       }
     }
